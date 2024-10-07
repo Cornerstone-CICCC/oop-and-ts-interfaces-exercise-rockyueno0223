b0293a3fbb007e4b49bedcb78393aea3c742ff25
Hiroki Ueno <rockyueno@gmail.com>
2024-10-07 16:24:21 -0700

    submit

--- dist/ex03.js
+++ dist/ex03.js
@@ -1,23 +1,16 @@
+"use strict";
 // Exercise 3: Optional Parameters & Properties
 // Define an interface for a "Car" object with an optional "color" property and a required "model" (string).
 // Create a function that takes this object as a parameter and logs the model and color (if available).
-
-interface Car {
-  model: string;
-  color?: string;
-}
-
-const describeCar = (car: Car): void => {
-  console.log(`Model: ${car.model}`);
-  if (car.color) {
-    console.log(`Color: ${car.color}`);
-  }
+const describeCar = (car) => {
+    console.log(`Model: ${car.model}`);
+    if (car.color) {
+        console.log(`Color: ${car.color}`);
+    }
 };
-
 describeCar({ model: "Tesla" });
 // Expected Output:
 // Model: Tesla
-
 describeCar({ model: "Ford", color: "Red" });
 // Expected Output:
 // Model: Ford
--- dist/ex07.js
+++ dist/ex07.js
@@ -1,21 +1,16 @@
+"use strict";
 // Exercise 7: Function Overloads
 // Define an interface for a function that can accept either a string or a number as a parameter.
 // Write a function using overloads that handles both types and logs a different message for each.
-
-interface Logger {
-  (input: string | number): void
-}
-
-const logMessage: Logger = (input) => {
-  if (typeof input === "string") {
-    console.log(`Message: ${input}`);
-  } else {
-    console.log(`Code: ${input}`);
-  }
+const logMessage = (input) => {
+    if (typeof input === "string") {
+        console.log(`Message: ${input}`);
+    }
+    else {
+        console.log(`Code: ${input}`);
+    }
 };
-
 logMessage("Hello");
 // Expected Output: "Message: Hello"
-
 logMessage(404);
 // Expected Output: "Code: 404"
